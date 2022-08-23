40e9b430c60ca2a189d00d67c8b87e69675555de
gtchappy <2425172709@qq.com>
2022-08-23 11:20:12 +0800

    8.23

---
+++ 11_布尔类型和比较运算符.py
@@ -0,0 +1,25 @@
+"""
+演示布尔类型的定义
+以及比较运算符的应用
+"""
+# 定义变量储存布尔类型的数据
+bool_1 = True
+bool_2 = False
+print(f"bool_1变量的内容是:{bool_1},类型是:{type(bool_1)}")
+print(f"bool_1变量的内容是:{bool_2},类型是:{type(bool_2)}")
+
+# 比较运算符的使用
+# ==,!=,>,<,<=,>=
+num1 = 10
+num2 = 10
+print(f"10==10的结果是:{num1 == num2}")
+
+num1 = 10
+num2 = 15
+print(f"10!=15的结果是:{num1!=num2}")
+
+name1 = "lala"
+name2 = "haha"
+print(f"lala==haha的结果是:{name1==name2}")
+
+# 演示大于小于,大于等于小于等于的比较运算
